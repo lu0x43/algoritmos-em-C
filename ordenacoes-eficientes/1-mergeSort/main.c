aab41db56d3e64004bf473961a2ef39b1a472a48
Lucas Caetano <lucas_caetano123@hotmail.com>
2022-09-05 23:24:41 -0300

    add rand array in  merge sort

--- ordenacoes-eficientes/1-mergeSort/main.c
+++ ordenacoes-eficientes/1-mergeSort/main.c
@@ -1,5 +1,18 @@
 #include "merge.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+int *randArray(int n, int seed, int max)
+{
+  srand(seed);
+  int *v = malloc(sizeof(int) * n);
+
+  for (int i = 0; i < n; i++)
+  {
+    v[i] = (rand() % max) + 1;
+  }
+  return v;
+}
 
 void printArray(int *v, int n)
 {
@@ -17,19 +30,27 @@ int main(int argc, char **argv)
 
   int v2[] = {2, 8, 6, 0, 1, 7};
   int n2 = sizeof(v2) / sizeof(int);
-  
-  int left = 0, right = 5;
 
-  printf("Size of array: %d \n\nVetor:", n);
-  printArray(v, n);
+  int numRand = atoi(argv[1]);
+  int *vRand = randArray(numRand, 99, numRand * 100);
 
-  printf("Merge:\n");
-  merge(v, 0, 2, 5);
-  printArray(v, n);
+  int left = 0, right = numRand - 1;
 
+  // printf("Size of array: %d \n\nVetor:", n);
+  // printArray(v, n);
+
+  // printf("Merge:\n");
+  // merge(v, 0, 2, 5);
+  // printArray(v, n);
+
+  printArray(vRand, numRand);
   printf("\nMerge Sort:\n");
-  mergeSort(v2, left, right);
-  printArray(v2, n2);
+
+  mergeSort(vRand, left, right);
+  printArray(vRand, numRand);
+
+  // mergeSort(v2, left, right);
+  // printArray(v2, n2);
 
   return 0;
 }
